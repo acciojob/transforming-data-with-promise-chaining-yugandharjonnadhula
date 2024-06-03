f855674a8ceac2d06cb9132c49aeffc82db0f758
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-06-03 20:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+const output document.getElementById("output");
+const input document.getElementById("ip");
+const btn document.getElementById("btn");
+
+btn.addEventListener('click', createInitialPromise);
+
+const delayPromise (value, delay) => new Promise((resolve, reject) => { 
+	setTimeout(() =>resolve(value), delay)
+
+})
+  function createInitialPromise() {
+  	const userTypedValue=input.value;
+  
+	delayPromise(userTypedValue, 2000) //4
+
+      .then((result) =>{
+		  output.textContent ='Result: $(result).'; 
+          return delayPromise(result * 2,1000);
+    }). then((result) =>
+		  output.textContent = 'Result: $(result).';
+          return delayPromise(result - 3,1000);
+
+   }).then((result)=> {
+	  output.textContent ='Result: $(result).'; 
+      return delayPromise(result / 2,1000);
+   }).then((result)=> {
+	  output.textContent ='Result: $(result).'; 
+      return delayPromise(result + 10,1000);
+
+   }).then((final) => {
+	  output.textContent ='Final Result: $(Final).'; 
+     
+  })
+}
